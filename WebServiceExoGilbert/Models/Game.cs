2591b88279392ffd57257cca7b2db4281c5c7afe
AIGLE <damien_aigle@hotmail.com>
2020-07-21 08:49:29 +0200

    first commit

---
+++ WebServiceExoGilbert/Models/Game.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace WebServiceExoGilbert.Models
+{
+    public class Game
+    {
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string IdGame { get; set; }
+
+        [BsonElement("Name")]
+        public string Host_team { get; set; }
+        public string Guest_team { get; set; }
+        public DateTime Date { get; set; }
+        public string Note { get; set; }
+
+
+    }
+}
